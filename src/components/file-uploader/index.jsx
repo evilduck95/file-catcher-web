11bc771b8d069eec7431f39002f50ae920d7fe67
evilduck95 <keiranbrown95@gmail.com>
2024-03-10 00:14:13 +0000

    Started adding some file uploading using the new async processing backend.

--- src/components/file-uploader/index.jsx
+++ src/components/file-uploader/index.jsx
@@ -1,25 +1,47 @@
-import react, {useState} from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+import {useEffect, useState} from "react";
 
 const Heading = styled.div`
     font-size: large;
 `;
 
 
+const uploadFilmUrl = 'http://localhost:8200/film/upload';
+const processFilmUrl = 'http://localhost:8200/film/process';
+
 const FileUploader = ({headingText}) => {
 
-    const [file, setFile] = useState();
+    const [jobIds, setJobIds] = useState([]);
+
+    useEffect(() => {
+        console.log('State', jobIds);
+    }, [jobIds]);
 
     const onFileChange = (event) => {
-        setFile(event.target.files[0]);
-    };
-
-    const onFileUpload = () => {
+        let file = event.target.files[0];
         const formData = new FormData();
 
         formData.append('file', file, file.name);
         console.log('Form Data', formData, file);
+        axios.post(uploadFilmUrl, formData)
+            .then(
+                (succ) => {
+                    console.log('hooray', succ);
+                    const { jobId: responseJobId } = succ.data;
+                    setJobIds(currentIds => {
+                        currentIds.push(responseJobId);
+                        return currentIds;
+                    });
+                },
+                (fail) => console.log('fail', fail)
+            );
+    };
+
+    const onFileUpload = () => {
+        console.log('submit', jobIds);
+        axios.patch(processFilmUrl, { jobIds })
+            .then(console.log, console.log);
     }
 
     return (
