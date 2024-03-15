6e96c60b006a6484836e08d815666b4ff603d39f
evilduck95 <keiranbrown95@gmail.com>
2024-03-15 01:25:37 +0000

    Added drag drop file events and some styling.

--- src/components/FileListDisplay.jsx
+++ src/components/FileListDisplay.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import styled from "styled-components";
 import {ImCross} from "react-icons/im";
 
 const FilesListContainer = styled.div`
+    // Parent styles to allow overlay to respect boundaries.
+    float: left;
+    position: relative;
+    
     width: 400px;
     min-height: 500px;
     display: flex;
@@ -30,10 +34,71 @@ const Instruction = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-`
+`;
+
+const DragOverOverlay = styled.div`
+    // Respect your parent's boundaries
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    // Max size
+    width: 100%;
+    height: 100%;
+
+    background: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPScxMCcgaGVpZ2h0PScxMCc+CiAgPHJlY3Qgd2lkdGg9JzEwJyBoZWlnaHQ9JzEwJyBmaWxsPSdibGFjaycvPgogIDxwYXRoIGQ9J00tMSwxIGwyLC0yCiAgICAgICAgICAgTTAsMTAgbDEwLC0xMAogICAgICAgICAgIE05LDExIGwyLC0yJyBzdHJva2U9J3doaXRlJyBzdHJva2Utd2lkdGg9JzEnLz4KPC9zdmc+") repeat;
+    opacity: 25%;
+`;
 
 const FileListDisplay = ({files, fileAddCallback, removeCallback}) => {
-    console.log('render list');
+
+    const listContainerRef = useRef(null);
+    const dragOverlayRef = useRef(null);
+
+    const [draggingFile, setDraggingFile] = useState(false);
+
+    useEffect(() => {
+        listContainerRef.current.addEventListener('dragover', handleDragOver);
+        listContainerRef.current.addEventListener('drop', handleDrop);
+        listContainerRef.current.addEventListener('dragenter', handleDragEnter);
+        listContainerRef.current.addEventListener('dragleave', handleDragLeave);
+        return () => {
+            listContainerRef.current.removeEventListener('dragover', handleDragOver);
+            listContainerRef.current.removeEventListener('drop', handleDrop);
+            listContainerRef.current.removeEventListener('dragenter', handleDragEnter);
+            listContainerRef.current.removeEventListener('dragleave', handleDragLeave);
+        }
+    }, []);
+
+    const handleDragEnter = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        if (event.target !== dragOverlayRef.current) {
+            console.log('enter');
+            setDraggingFile(true);
+        }
+    };
+
+    const handleDragLeave = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        if (event.target === dragOverlayRef.current) {
+            console.log('leave');
+            setDraggingFile(false);
+        }
+    }
+
+    const handleDragOver = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log('drag over');
+    };
+
+    const handleDrop = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log('drop');
+    };
 
     const fileRemoved = (index) => {
         console.log(`${index} removed`)
@@ -41,9 +106,14 @@ const FileListDisplay = ({files, fileAddCallback, removeCallback}) => {
     }
 
     return (
-        <FilesListContainer>
-            {files.map((f, i) => <FileRow style={{cursor: 'pointer'}}><div>{f.name}</div><ImCross className={'clickable-icon'} onClick={() => fileRemoved(i)}/></FileRow>)}
-            <FileRow style={{cursor: 'pointer'}} onClick={fileAddCallback}><Instruction>Drop files here (Coming Soon!) or click to select</Instruction></FileRow>
+        <FilesListContainer ref={listContainerRef}>
+            {draggingFile && <DragOverOverlay ref={dragOverlayRef}>
+            </DragOverOverlay>}
+            {files.map((f, i) => <FileRow style={{cursor: 'pointer'}}>
+                <div>{f.name}</div>
+                <ImCross className={'clickable-icon'} onClick={() => fileRemoved(i)}/></FileRow>)}
+            <FileRow style={{cursor: 'pointer'}} onClick={fileAddCallback}><Instruction>Drop files here (Coming
+                Soon!) or click to select</Instruction></FileRow>
         </FilesListContainer>
     )
 };
